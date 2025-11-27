28c351223a38690902a64b4dd766fa0fb4f4b7b8
Uladzislau Navitski <vladnovik123456789@gmail.com>
2025-11-27 17:25:17 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let chapters = document.querySelectorAll("li.chapters-list__item");
+let chaptersInfo = document.querySelectorAll(".chapters__info");
+console.log(chapters);
+console.log(chaptersInfo);
+chapters.forEach(chapter => {
+    chapter.addEventListener("click", () => {
+        chapters.forEach(e => e.classList.remove("active"));
+        chapter.classList.add("active");
+        chaptersInfo.forEach(e => e.classList.remove("chapters-active"));
+        document.getElementById(chapter.id+"__info").classList.add("chapters-active")
+    })
+})
+
+let faqQuestions = document.querySelectorAll(".faq__question-wrapper");
+faqQuestions.forEach(e => {
+    e.addEventListener("click", () => {
+        e.classList.toggle("faq__question-wrapper_active");
+        e.querySelector(".faq__answer-wrapper").classList.toggle("faq__answer-wrapper_active");
+        e.querySelector(".faq__question").classList.toggle("faq__question_active");
+    })
+})
